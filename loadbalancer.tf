a73d4f92b26072e2f64fd118139158f5d0e8ee52
Felix-aigner <33003840+Felix-aigner@users.noreply.github.com>
2020-10-13 17:59:10 +0200

    added loadGen.sh

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -7,18 +7,18 @@ resource "exoscale_nlb" "nlb" {
 resource "exoscale_nlb_service" "healthchecker" {
   zone = exoscale_nlb.nlb.zone
   name = "healthchecker"
-  description = "http healthchecker"
+  description = "healthcheck"
   nlb_id = exoscale_nlb.nlb.id
   instance_pool_id = exoscale_instance_pool.instancepool.id
   protocol = "tcp"
   port = 80
-  target_port = 80
+  target_port = 8080
   strategy = "round-robin"
 
   healthcheck {
-    port = 80
+    port = 8080
     mode = "http"
-    uri = "/"
+    uri = "/health"
     interval = 5
     timeout = 3
     retries = 1
